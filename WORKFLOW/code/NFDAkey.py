9fb76c84afa645b37d478e112a8220c80b307cab
Josep Gao <hitmy2013@hotmail.com>
2017-09-05 09:58:18 +0800

    Key system patched
    With the NFDAkey.py file and the run_NFDA.sh, to realize the promote calculation with developer mode or user mode

---
+++ WORKFLOW/code/NFDAkey.py
@@ -0,0 +1,41 @@
+# NFDAkey.py
+keycontent = '4rfvbnm'
+import os
+import sys
+
+locipe = os.environ['SSH_CONNECTION'][10:12]
+
+if locipe == '21':
+    username = os.environ["USERNAME"]
+    if username == 'GaoMY':
+        print(' ')
+        print(' ')
+        print('    $$$ WELCOME to MLTF(v0.1) ! $$$')
+        print('    <Author: Josep GAO> ')
+        print('    <Copyright Reserved.>')
+        print(' ')
+        print(' ')
+        pscode = input('    USERNAME is [GaoMY], PLEASE INPUT PASSCODE and PRESS ENTER...')
+        if pscode == keycontent:
+            print(' ')
+            print(' ')
+            print('    ' + '$' * 20)
+            print('    $ Wlkm! Commander! $')
+            print('    ' + '$' * 20)
+            sys.exit(1)
+        else:
+            print(' ')
+            print(' ')
+            print('    You enter a wrong passcode, EXIT! Please CHANGE YOUR USERNAME in run_NFDA.sh and re-run it! ')
+            print(' ')
+            sys.exit(0)
+    else:
+        print(' ')
+        print(' ')
+        print('    $$$ WELCOME to MLTF(v0.1) ! $$$')
+        print('    <Author: Josep GAO> ')
+        print('    <Copyright Reserved.>')
+        print(' ')
+        print(' ')
+        input('    USERNAME is [' + username + '], PRESS ENTER to Confirm and start! ')
+        sys.exit(1)
